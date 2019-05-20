b755ee0dfa8e23989e0f0d430ea71dbf3dc3bbf5
Gabriël Ramaker <gabriel@lingewoud.nl>
2019-05-20 15:58:08 +0200

    added caching

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+//  Copyright notice
+//
+//  (c) 2019 Gabriël Ramaker <gabriel@lingewoud.nl>, Lingewoud
+//
+//  All rights reserved
+//
+//  This script is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  The GNU General Public License can be found at
+//  http://www.gnu.org/copyleft/gpl.html.
+//
+//  This script is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  This copyright notice MUST APPEAR in all copies of the script!
+
+var connector                 = connector || {};
+var connector.cacheExpTime    = 60;
+var connector.cacheTag        = 'JSONResults';
+
 function sendUserError( message ) {
   var cc = DataStudioApp.createCommunityConnector();
       cc.newDebugError()
@@ -9,17 +34,21 @@ function getAuthType() {
   return { type: 'NONE' };
 }
 
+function isAdminUser() {
+  return false;
+}
+
 function getConfig( request ) {
   var cc      = DataStudioApp.createCommunityConnector();
   var config  = cc.getConfig();
 
   config.newInfo()
     .setId( 'instructions' )
-    .setText( 'Fetch JSON data' );
+    .setText( 'Fill out the form to connect to a JSON data source. Nested JSON data is not supported. ' );
 
   config.newTextInput()
     .setId( 'url' )
-    .setName( 'Enter the JSON URL' )
+    .setName( 'Enter the URL of a JSON data source' )
     .setHelpText( 'e.g. https://my-url.org/json')
     .setPlaceholder( 'https://my-url.org/json' );
 
@@ -28,10 +57,28 @@ function getConfig( request ) {
   return config.build();
 }
 
+function getCachedData( url ) {
+  var cache       = CacheService.getUserCache();
+  var cachedData  = cache.get( connector.cacheKey );
+
+  if ( cachedData !== null ) {
+    var response = cachedData;
+  } else {
+    try {
+      var response = UrlFetchApp.fetch( url );
+    } catch (e) {
+      sendUserError( '"' + url + '" returned an error' );
+    }
+
+    cache.put(connector.cacheKey, response, connector.cacheExpiration);
+  }
+  return response;
+}
+
 function fetchData( url ) {
   if ( !url || !url.match( /^https?:\/\/.+$/g ) ) sendUserError( '"' + url + '" is not a valid url.' );
 
-  var response  = UrlFetchApp.fetch( url );
+  var response  = getCachedData( url );
   var content   = JSON.parse( response )
 
   if ( !content ) sendUserError( '"' + url + '" returned no content.' );
